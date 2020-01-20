bd0dfd1c414e1448015a821be5a287ab8294b98d
Brandon Shankar <bshankar12@comcast.net>
2020-01-19 17:17:02 -0800

    auth code in server.js

--- server.js
+++ server.js
@@ -4,23 +4,42 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var session = require("express-session");
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+
 // Requiring our models for syncing
 var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Static directory
 app.use(express.static("public"));
+
+//create express for middleware for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // Routes
 // =============================================================
 require("./routes/creategroup-api-routes.js")(app);
 require("./routes/joingroup-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
